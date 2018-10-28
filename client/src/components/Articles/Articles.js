b8b8e33a9ceedf848c8b454c5fb11191874de8ee
Roger <rdpender3@gmail.com>
2018-10-27 20:37:04 -0700

    skeleton there for popover

--- client/src/components/Articles/Articles.js
+++ client/src/components/Articles/Articles.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import "./Articles.css"
 import { withStyles } from '@material-ui/core/styles';
 import Btn from '@material-ui/core/Button'
-import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
+// import Popover from '@material-ui/core/Popover';
+// import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 
 const styles = theme => ({
@@ -15,9 +15,9 @@ const styles = theme => ({
 class Article extends Component {
 
 
-    state = {
-        anchorEl: null,
-    };
+    // state = {
+    //     anchorEl: null,
+    // };
 
 
 
@@ -25,24 +25,24 @@ class Article extends Component {
          this.props.saved ? this.props.deleteArticle(id) : this.props.saveArticle(id);
     }
 
-    handleClick = event => {
-        this.setState({
-            anchorEl: event.currentTarget,
-        });
-    };
+    // handleClick = event => {
+    //     this.setState({
+    //         anchorEl: event.currentTarget,
+    //     });
+    // };
 
 
-    handleClose = () => {
-        this.setState({
-            anchorEl: null,
-        });
-    };
+    // handleClose = () => {
+    //     this.setState({
+    //         anchorEl: null,
+    //     });
+    // };
 
     showArticles = () => {
 
-        const { classes } = this.props;
-        const { anchorEl } = this.state;
-        const open = Boolean(anchorEl);
+        // const { classes } = this.props;
+        // const { anchorEl } = this.state;
+        // const open = Boolean(anchorEl);
 
         const { articles } = this.props;
         return articles.map((a, id) => (
@@ -50,14 +50,14 @@ class Article extends Component {
                 <a href={a.url} target="_blank" rel="noreferrer noopener"><h3>{a.title}</h3></a>
                 <h6>{a.snippet}</h6>
                 <Btn
-                    aria-owns={open ? 'simple-popper' : null}
-                    aria-haspopup="true"
+                    // aria-owns={open ? 'simple-popper' : null}
+                    // aria-haspopup="true"
                     variant="contained"
                     color="secondary"
-                    onClick={ this.props.saved ? this.clicked.bind(this, id) && this.handleClick : this.clicked.bind(this, id) && this.handleClick }>
+                    onClick={  this.props.saved ? this.clicked.bind(this, id)  : this.clicked.bind(this, id) }>
                     {this.props.saved ? 'Delete' : 'Save'}
                 </Btn>
-                <Popover
+                {/* <Popover
                     id="simple-popper"
                     open={open}
                     anchorEl={anchorEl}
@@ -72,7 +72,7 @@ class Article extends Component {
                     }}
                 >
                     <Typography className={classes.typography} >{this.props.saved ? 'Article Deleted!' : 'Article Saved!'}</Typography>
-                </Popover>
+                </Popover> */}
             </div>
         ));
     };
